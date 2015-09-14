62c1794af5d6220a70b7ae42158b2ce2493c279d
Peter Young <young40@gmail.com>
2015-09-15 00:51:45 +0800

    Initial Commit

---
+++ EasyOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EasyOpenGL
+//
+//  Created by Peter Young on 9/15/15.
+//  Copyright © 2015 Peter Young. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
