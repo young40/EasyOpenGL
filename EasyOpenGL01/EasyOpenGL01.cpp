ca4a9168217c1e4456cb159c459cc9e8e6466627
Peter Young <young40@gmail.com>
2015-09-15 23:25:06 +0800

    add EasyOpenGL01

---
+++ EasyOpenGL01/EasyOpenGL01.cpp
@@ -0,0 +1,59 @@
+//
+//  main.cpp
+//  EasyOpenGL01
+//
+//  Created by Peter Young on 9/15/15.
+//  Copyright © 2015 Peter Young. All rights reserved.
+//
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+extern "C"
+{
+    static void glfw_error_callback(int error, const char *desc)
+    {
+        fprintf(stderr, "Running got error: %d,\t %s\n", error, desc);
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    GLFWwindow *window;
+    
+    if(!glfwInit())
+    {
+        return -1;
+    }
+    
+    glfwSetErrorCallback(glfw_error_callback);
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    window = glfwCreateWindow(960, 640, "Tutorial 01", nullptr, nullptr);
+    if(!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    
+    cout << glGetString(GL_VENDOR) << endl;
+    cout << glGetString(GL_RENDERER) << endl;
+    cout << glGetString(GL_VERSION) << endl;
+    
+    
+    while (!glfwWindowShouldClose(window)) {
+        //        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    
+    return 0;
+}
