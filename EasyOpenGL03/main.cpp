32ac781c4e21918974fc79a77f4bce1105bafb49
Peter Young <young40@gmail.com>
2017-12-03 22:41:04 +0800

    重新添加EasyOpenGL 03

---
+++ EasyOpenGL03/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EasyOpenGL03
+//
+//  Created by Peter Young on 03/12/2017.
+//  Copyright © 2017 Peter Young. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
