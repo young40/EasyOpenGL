385c63f23061f1a201708536c307a779cab8dacd
Peter Young <young40@gmail.com>
2017-12-03 23:02:34 +0800

    update

--- EasyOpenGL03/EasyOpenGL03.cpp
+++ EasyOpenGL03/EasyOpenGL03.cpp
@@ -8,8 +8,15 @@
 
 #include <iostream>
 
+//#include <OpenGL/gl3.h> //否则会找不到 glGenVertexArrays
+//#include <GLFW/glfw3.h>
+
+#include "EasyOpenGL.hpp"
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    GLFWwindow *window = EO_CreateWindow(960, 640, "轻轻松松OpenGL 03 -- 渐变色三角形");
+    
+    
+    
     return 0;
 }
