f27229e31e7eb387cbe5cbb88ee8f378b5b3135a
Peter Young <young40@gmail.com>
2017-12-17 09:06:45 +0800

    EasyOpenGL06

--- Common/EasyOpenGL.hpp
+++ Common/EasyOpenGL.hpp
@@ -14,8 +14,10 @@
 #include <string>
 #include <iostream>
 
-#include <GLFW/glfw3.h>
-#include <GLFW/glfw3native.h>
+#include "EasyOpenGL.hpp"
+
+#include "GLFW/glfw3.h"
+#include "GLFW/glfw3native.h"
 
 using namespace std;
 
---
+++ EasyOpenGL06/EasyOpenGL06.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  EasyOpenGL06
+//
+//  Created by Peter Young on 17/12/2017.
+//  Copyright © 2017 Peter Young. All rights reserved.
+//
+
+#include <iostream>
+#include "EasyOpenGL.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
