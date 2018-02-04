6e73503d8a239083c4c2332275a0fe3813dfb34d
Peter Young <young40@gmail.com>
2018-02-04 09:02:52 +0800

    新增EasyOpenGL 12

---
+++ EasyOpenGL12/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EasyOpenGL12
+//
+//  Created by Peter Young on 04/02/2018.
+//  Copyright © 2018 Peter Young. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
